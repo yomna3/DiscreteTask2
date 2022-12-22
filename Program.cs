4d9c19434f3e4e7576ab82f05fc92abd760816ea
yomna3 <121171028+yomna3@users.noreply.github.com>
2022-12-22 17:13:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write(" Enter the Minimum number : ");
+            int N1 = int.Parse(Console.ReadLine());
+            Console.Write(" Enter the Maximum number : ");
+            int N2 = int.Parse(Console.ReadLine());
+            for (int x = N1; x <= N2; x++)
+            {
+                int sum = 0;
+                for (int y = 1; y <= x/2; y++)
+                {
+
+                    if (x % y == 0)
+                    {
+
+                        sum += y;
+
+                    }
+
+                }
+                if (sum == x)
+                {
+
+                    Console.WriteLine(x + " is prefect ");
+
+                }
+            }
+
+
+        }
+    }
+}
